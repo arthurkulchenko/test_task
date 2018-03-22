cc58f98b99b59a4fdcac241d1eda2539fe552cb0
Megalapa <megalapa@MBA.local>
2018-03-22 16:55:33 +0300

    done

--- app/services/selenium_call.rb
+++ app/services/selenium_call.rb
@@ -1,24 +1,23 @@
-require 'headless'
-require 'selenium-webdriver'
-
 class SeleniumCall
-  
+
   def self.find_out_message_amount forum_address, login, password
     headless = Headless.new
     headless.start
-    wait = Selenium::WebDriver::Wait.new(:timeout => 100)
+    wait   = Selenium::WebDriver::Wait.new(:timeout => 100)
     driver = Selenium::WebDriver.for :firefox, driver_path: 'public/mac/geckodriver'
+    
     driver.navigate.to forum_address
-    login_field = driver.find_element :name => "user"
-    login_field.send_keys login
+    
+    login_field    = driver.find_element :name => "user"
     password_field = driver.find_element :name => "passwd"
+    
+    login_field.send_keys login
     password_field.send_keys password
     password_field.submit
     
     wait.until do
       driver.find_element(xpath: "/html/body/div[2]/div[3]/a[2]").attribute('innerHTML')
     end
-    
   end
 
 end
